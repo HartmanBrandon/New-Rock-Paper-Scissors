5a9e6c84cacb83b88f5a0a1b357020df9b91e86e
HartmanBrandon <brandon@jokerbusinesssolutions.com>
2024-02-29 16:13:23 -0800

    Used Prettier to reformat the code

--- script.js
+++ script.js
@@ -26,35 +26,32 @@ function playGame() {
   let decision = prompt("Do you wish to play again? Y/N: ").toUpperCase();
 
   // Might make the decision making its own function that
-  while(decision == 'Y'){
-  if (
-    (playerSelection == "rock" && computerChoice == "rock") ||
-    (playerSelection == "paper" && computerChoice == "paper") ||
-    (playerSelection == "scissors" && computerChoice == "scissors")
-  ) {
-    console.log("Oh no. You all picked the same thing.");
-    console.log(decision);
-
-  } else if (
-    (playerSelection == "rock" && computerChoice == "scissors") ||
-    (playerSelection == "paper" && computerChoice == "rock") ||
-    (playerSelection == "scissors" && computerChoice == "paper")
-  ) {
-    console.log(`${playerSelection} beats ${getComputerChoice}`);
-    console.log(decision);
+  while (decision == "Y") {
+    if (
+      (playerSelection == "rock" && computerChoice == "rock") ||
+      (playerSelection == "paper" && computerChoice == "paper") ||
+      (playerSelection == "scissors" && computerChoice == "scissors")
+    ) {
+      console.log("Oh no. You all picked the same thing.");
+      console.log(decision);
+    } else if (
+      (playerSelection == "rock" && computerChoice == "scissors") ||
+      (playerSelection == "paper" && computerChoice == "rock") ||
+      (playerSelection == "scissors" && computerChoice == "paper")
+    ) {
+      console.log(`${playerSelection} beats ${getComputerChoice}`);
+      console.log(decision);
+    } else if (
+      (computerChoice == "rock" && playerSelection == "scissors") ||
+      (computerChoice == "paper" && playerSelection == "rock") ||
+      (computerChoice == "scissors" && playerSelection == "paper")
+    ) {
+      console.log(`${computerChoice} beats ${playerSelection}`);
+      console.log(decision);
+    } else {
+      console.log("Thank you for playing.");
+    }
   }
-  else if (
-    (computerChoice == "rock" && playerSelection == "scissors") ||
-    (computerChoice == "paper" && playerSelection == "rock") ||
-    (computerChoice == "scissors" && playerSelection == "paper")
-  ) {
-    console.log(`${computerChoice} beats ${playerSelection}`);
-    console.log(decision);
-  }
-  else {
-    console.log("Thank you for playing.");
-  }
-}
 }
 
 playGame();
